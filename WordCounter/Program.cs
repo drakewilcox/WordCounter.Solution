a13fd77caff23602689b6b7fddda8f718159744a
Drake Wilcox <drakewilcox@gmail.com>
2020-03-06 08:25:28 -0800

    add boilerplate to project.

---
+++ WordCounter/Program.cs
@@ -0,0 +1,14 @@
+using WordCounter.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Counter
+{
+  public class Program
+  {
+    public static void Main()
+    {
+
+    }
+  }
+}
